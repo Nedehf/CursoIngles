d95718d8efc5bd54ff57486c7ac752f83866859b
hanielhgc <haniel_sasuke@hotmail.com>
2017-03-15 14:43:13 -0300

    Update AlunoDao.java

--- src/persistence/AlunoDao.java
+++ src/persistence/AlunoDao.java
@@ -2,7 +2,32 @@ package persistence;
 
 public class AlunoDao {
 	
-	public void inserir(){
+	private Connection conexao;
+	
+	public AlunoDao{
+	this.conexao = new ConnectionFactory().getConnection();
+		
+	}
+	
+	public void inserir(Aluno aluno){
+		
+		String sql = "insert into aluno" + "(cpf, nome, email)" + "values (?, ?, ?)";
+		
+		try{
+		
+			PreparedStatement pstmt = conexao.prepareStatement(sql);
+			
+			pstmt.setString(1, aluno.getCpf());
+			pstmt.setString(2, aluno.getNome());
+			pstmt.setString(3, aluno.getEmail());
+			
+			pstmt.execute();
+			pstmt.close();
+			
+			
+		}catch (SQLException e){
+			throw new RuntimeException(e);	
+		}
 		
 	}
 
