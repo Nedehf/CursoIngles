d50570cac4830be3077135e2d3020fbdb8d79ed9
nedehf <nedehf@edu.unifor.br>
2017-03-16 08:27:37 -0300

    Atualização do Dao

--- src/persistence/AlunoDao.java
+++ src/persistence/AlunoDao.java
@@ -1,47 +1,50 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import beans.Aluno;
+
 public class AlunoDao {
-	
+
 	private Connection conexao;
-	
-	public AlunoDao{
-	this.conexao = new ConnectionFactory().getConnection();
-		
-	}
-	
-	public void inserir(Aluno aluno){
-		
+
+//	public AlunoDao() {
+//		this.conexao = new ConnectionFactory().getConnection();
+//
+//	}
+
+	public void inserir(Aluno aluno) {
+
 		String sql = "insert into aluno" + "(cpf, nome, email)" + "values (?, ?, ?)";
-		
-		try{
-		
+
+		try {
+
 			PreparedStatement pstmt = conexao.prepareStatement(sql);
-			
+
 			pstmt.setString(1, aluno.getCpf());
 			pstmt.setString(2, aluno.getNome());
 			pstmt.setString(3, aluno.getEmail());
-			
+
 			pstmt.execute();
 			pstmt.close();
-			
-			
-		}catch (SQLException e){
-			throw new RuntimeException(e);	
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-		
+
 	}
 
-	public void remover(){
-		
+	public void remover() {
+
 	}
 
-	public void alterar(){
-		
+	public void alterar() {
+
 	}
 
-	public void mostrar(){
-		
-	}
+	public void mostrar() {
 
+	}
 
 }
