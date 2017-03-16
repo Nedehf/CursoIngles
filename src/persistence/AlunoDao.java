58eb0619ceac3c158fe7015d153860c029d1d73e
nedehf <nedehf@edu.unifor.br>
2017-03-16 11:34:44 -0300

    Fix do AlunoDao

--- src/persistence/AlunoDao.java
+++ src/persistence/AlunoDao.java
@@ -9,25 +9,28 @@ public class AlunoDao {
 
 	public void inserir(Aluno aluno) {
 		Connection conexao = null;
-		String sql = "insert into aluno" + "(cpf, nome, email)" + "values (?, ?, ?)";
+		PreparedStatement pstmt = null;
+		
+		String sql = "insert into aluno (cpf, nome, email) values (?, ?, ?)";
 
 		try {
 			conexao = Conexao.getConnection();
-			PreparedStatement pstmt = conexao.prepareStatement(sql);
+			pstmt = conexao.prepareStatement(sql);
 
 			pstmt.setString(1, aluno.getCpf());
 			pstmt.setString(2, aluno.getNome());
 			pstmt.setString(3, aluno.getEmail());
 
-			pstmt.execute();
-			pstmt.close();
+			pstmt.executeUpdate();
+		//	pstmt.close();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new RuntimeException(e);
+		//	throw new RuntimeException(e);
 		} finally {
 			try {
-				if (conexao != null)
+		//		if (conexao != null)
+					pstmt.close();
 					conexao.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
