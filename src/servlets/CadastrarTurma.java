a522be66fdfc1a8e92cbbd3388d13c2a927e4b8e
nedehf <nedehf@edu.unifor.br>
2017-03-26 13:31:58 -0300

    Cadastro de Turmas (Html & Servlet)

--- src/servlets/CadastrarTurma.java
+++ src/servlets/CadastrarTurma.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Turma;
+import persistence.TurmaDao;
+
 @WebServlet("/CadastrarTurma")
 public class CadastrarTurma extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,29 +21,35 @@ public class CadastrarTurma extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
+		
 		String cod = request.getParameter("codigo");
 		String lvl = request.getParameter("nivel");
 		String prof = request.getParameter("professor");
 		String hr = request.getParameter("horario");
 		String sala = request.getParameter("sala");
-		String qtde = request.getParameter("Message");
+		int qtde = Integer.parseInt(request.getParameter("quantidade"));
 		String sts = request.getParameter("status");
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("Codigo: "+cod+"<br/>");
-		out.println("Nivel: "+lvl+"<br/>");
-		out.println("Professor: "+prof+"<br/>");
-		out.println("Horario: "+hr+"<br/>");
-		out.println("Sala: "+sala+"<br/>");
-		out.println("Quantidade Maxima: "+qtde+"<br/>");
-		out.println("Status: "+sts+"<br/>");
-		out.println("</body></html>");
-		out.close();
+		Turma turma = new Turma(cod,lvl,prof,hr,sala,qtde,sts);
+		TurmaDao tdao = new TurmaDao();
+		
+		tdao.inserir(turma);
+		
+		
+//		PrintWriter out = response.getWriter();
+//		out.println("<html><body>");
+//		out.println("Codigo: "+cod+"<br/>");
+//		out.println("Nivel: "+lvl+"<br/>");
+//		out.println("Professor: "+prof+"<br/>");
+//		out.println("Horario: "+hr+"<br/>");
+//		out.println("Sala: "+sala+"<br/>");
+//		out.println("Quantidade Maxima: "+qtde+"<br/>");
+//		out.println("Status: "+sts+"<br/>");
+//		out.println("</body></html>");
+//		out.close();
 		
 
-	//	response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
