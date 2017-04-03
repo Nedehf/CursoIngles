cc2b437983bb97fd88f74dec049210401f033b9d
nedehf <nedehf@edu.unifor.br>
2017-04-03 01:50:45 -0300

    Matricula
    Falta a verificação

--- src/servlets/MatricularAluno.java
+++ src/servlets/MatricularAluno.java
@@ -1,8 +1,7 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,12 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import beans.Aluno;
-import beans.Turma;
-import persistence.TurmaDao;
-
-import beans.Aluno;
-import persistence.AlunoDao;
+import beans.Matricula;
+import persistence.MatriculaDao;
 
 @WebServlet("/MatricularAluno")
 public class MatricularAluno extends HttpServlet {
@@ -29,97 +24,22 @@ public class MatricularAluno extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-	
-		TurmaDao tdao = new TurmaDao();
-		List<Turma> turmas = tdao.mostrar();
+		String cpf = request.getParameter("Aluno");
+		String cod = request.getParameter("Turma");
+		Date date = new Date();
 
-		AlunoDao adao = new AlunoDao();
+//		AlunoDao adao = new AlunoDao();
+//		Aluno al = adao.buscarAluno(cpf);
+//		
+//		TurmaDao tdao = new TurmaDao();
+//		Turma t = tdao.buscar(cod);
 
-		List<Aluno> alunos = adao.mostrar();
-
-		PrintWriter out = response.getWriter();
-
-		out.println("<html>" + "<head>" + "<meta charset='ISO-8859-1'>" + "<title>Matricular Aluno</title>"
-				+ "<link rel='stylesheet' href='https://www.w3schools.com/w3css/3/w3.css'>" + "<link rel='stylesheet'"
-				+ "	href='https://fonts.googleapis.com/css?family=Montserrat'>" + "<link rel='stylesheet'"
-				+ "	href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>" +
-
-				"<title>Efetuar Matrícula</title>" + "</head>");
-
-		out.println("<style>" + "body, h1, h2, h3, h4, h5, h6 {" + "	font-family: 'Montserrat', sans-serif" + "}" +
-
-				"h3 {" + "	color: white;" + "}" +
-
-				"input[type='radio'] {" + "	margin: 0 10px 0 10px;" + "}" +
-
-				".w3-row-padding img {" + "	margin-bottom: 12px" + "}" +
-
-				"/* Set the width of the sidebar to 120px */" + ".w3-sidebar {" + "	width: 120px;"
-				+ "	background: #222;" + "}" +
-
-				"/* Add a left margin to the 'page content' that matches the width of the sidebar (120px) */"
-				+ "#main {" + "	margin-left: 120px" + "}" +
-
-				"/* Remove margins from 'page content' on small screens */"
-				+ "@media only screen and (max-width: 600px) {" + "	#main {" + "		margin-left: 0" + "	}" + "}" +
-
-				".bgimg {" + "	background-position: center;" + "	background-repeat: no-repeat;"
-				+ "	background-size: cover;" + "	background-image: url('Resources\\BG_01.jpg');"
-				+ "	min-height: 100%;" + "}" + "</style>");
-
-		out.println("<body class='w3-black'>" +
-
-				"<nav class='w3-sidebar w3-hide-medium w3-hide-small' style='width: 30%'>"
-				+ "	<div class='bgimg'></div>" + "</nav>");
-
-		out.println("<!-- Page Content -->" + "<div class='w3-main w3-padding-large' style='margin-left: 29%'>" +
-
-				"	<div class='w3-padding-64 w3-content w3-text-grey' id='cadastro'"
-				+ "		style='margin-bottom: 64px'>" + "		<h2 class='w3-text-light-grey'>Efetuar Matrícula</h2>"
-				+ "		<hr style='width: 200px' class='w3-opacity'>" +
-
-				"		<!-- Campos -->" + "		<p>Selecione o aluno e a turma a ser matriculado:</p>" +
-
-				"		<form action='MatriculaAluno' method='post'>");
-
-		out.println("<!-- Combobox de Aluno -->" + "			<p>"
-				+ "	<select class='w3-input w3-padding-16 w3-border' required" + "		name='cpfAluno' >" +
-
-				"		<option class='w3-input w3-padding-16 w3-border' value='' disabled"
-				+ "			selected>Aluno</option>");
-
-		for (Aluno aluno : alunos) {
-			out.println("<option value='" + aluno.getCpf() + "'>" + aluno.getNome() + "</option>");
-		}
-		out.println("</select>");
-
-		out.println("</p>");
-
-		out.println("<!-- Combobox de Turma -->" + "			<p>"
-				+ "	<select class='w3-input w3-padding-16 w3-border' required" + "		name='codigo' >" +
-
-				"		<option class='w3-input w3-padding-16 w3-border' value='' disabled"
-				+ "			selected>Turma</option>");
-
-		for (Turma turma : turmas) {
-			out.println("<option value='" + turma.getCodigo() + "'>" + turma.getCodigo() + "</option>");
-		}
-		out.println("</select>");
-
-		out.println("</p>");
-
-		out.println("<!-- Submit -->" + "<p>" + "	<button class='w3-button w3-padding-large' type='submit'>"
-				+ "		<i class='fa fa-paper-plane'></i> Matricular" + "	</button>" + "</p>");
-		out.println("</form>");
-		out.println("<p>"+
-		"<a href='Index.html'>Voltar ao Início</a>"+
-		"</p>");
-		out.println("</div>");
-
-		out.println("<!-- END PAGE -->" + "</div>");
-
-		out.println("</body>");
-		out.println("</html>");
+		MatriculaDao mdao = new MatriculaDao();
+		Matricula m = new Matricula(cpf,cod,date,0,1);
+		
+		mdao.inserir(m);
+		
+//		PrintWriter out = response.getWriter();
 
 	}
 
---
+++ src/servlets/PreparaMatricula.java
@@ -0,0 +1,145 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Aluno;
+import beans.Turma;
+import persistence.AlunoDao;
+import persistence.TurmaDao;
+
+@WebServlet("/PreparaMatricula")
+public class PreparaMatricula extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		TurmaDao tdao = new TurmaDao();
+		List<Turma> turmas = tdao.mostrar();
+
+		AlunoDao adao = new AlunoDao();
+
+		List<Aluno> alunos = adao.mostrar();
+
+		PrintWriter out = response.getWriter();
+
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='ISO-8859-1'>");
+		out.println("<link rel='stylesheet' href='https://www.w3schools.com/w3css/3/w3.css'>");
+		out.println("<link rel='stylesheet'");
+		out.println("	href='https://fonts.googleapis.com/css?family=Montserrat'>");
+		out.println("<link rel='stylesheet'");
+		out.println("	href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>");
+
+		out.println("<title>Matricula</title>");
+		out.println("</head>");
+
+		out.println("<style>");
+		out.println("body, h1, h2, h3, h4, h5, h6 {");
+		out.println("font-family: 'Montserrat', sans-serif");
+		out.println("}");
+
+		out.println(".w3-row-padding img {");
+		out.println("margin-bottom: 12px");
+		out.println("}");
+
+		out.println("/* Set the width of the sidebar to 120px */ .w3-sidebar {");
+		out.println("width: 120px;");
+		out.println("background: #222;");
+		out.println("}");
+
+		out.println(
+				"/* Add a left margin to the 'page content' that matches the width of the sidebar (120px) */ #main {");
+		out.println("margin-left: 120px");
+		out.println("}");
+
+		out.println(
+				"/* Remove margins from 'page content' on small screens */ @media only screen and (max-width: 600px) {");
+		out.println("#main {");
+		out.println("	margin-left: 0");
+		out.println("}");
+		out.println("}");
+
+		out.println(".bgimg {");
+		out.println("background-position: center;");
+		out.println("background-repeat: no-repeat;");
+		out.println("background-size: cover;");
+		out.println("background-image: url('Resources\\BG_01.jpg');");
+		out.println("min-height: 100%;");
+		out.println("}");
+		out.println("</style>");
+
+		out.println("<body class='w3-black'>");
+
+		out.println("<nav class='w3-sidebar w3-hide-medium w3-hide-small' style='width: 30%'>");
+		out.println("	<div class='bgimg'></div>");
+		out.println("</nav>");
+
+		out.println("<!-- Page Content -->");
+		out.println("<div class='w3-main w3-padding-large' style='margin-left: 29%'>");
+
+		out.println("	<div class='w3-padding-64 w3-content w3-text-grey' id='matricula'");
+		out.println("		style='margin-bottom: 64px'>");
+		out.println("		<h2 class='w3-text-light-grey'>Matricula</h2>");
+		out.println("		<hr style='width: 200px' class='w3-opacity'>");
+
+		out.println("		<!-- Campos -->");
+		out.println("		<p>Preencha os campos abaixo:</p>");
+
+		out.println("		<form action='MatricularAluno' method='post'>");
+
+		out.println("			<!-- Alunos (ComboBox) -->");
+		out.println("			<p>");
+		out.println("				<select class='w3-select w3-padding-16 w3-border' required name='Aluno'>");
+		out.println("					<option value='' disabled selected>Aluno</option>");
+		for (Aluno aluno : alunos) {
+			out.println("<option value='" + aluno.getCpf() + "'>" + aluno.getNome() + " - CPF: " + aluno.getCpf()
+					+ "</option>");
+		}
+		out.println("				</select>");
+		out.println("			</p>");
+
+		out.println("			<!-- Turmas (ComboBox) -->");
+		out.println("			<p>");
+		out.println("				<select class='w3-select w3-padding-16 w3-border' required name='Turma'>");
+		out.println("					<option value='' disabled selected>Turma</option>");
+		for (Turma turma : turmas) {
+			out.println("<option value='" + turma.getCodigo() + "'>" + turma.getNivel() + " - " + turma.getHorario()
+					+ " (" + turma.getCodigo() + ")" + "</option>");
+		}
+		out.println("			</p>");
+
+		out.println("			<!-- Submit -->");
+		out.println("			<p>");
+		out.println("				<button class='w3-button w3-padding-large' type='submit'>");
+		out.println("					<i class='fa fa-paper-plane'></i> Matricular");
+		out.println("				</button>");
+		out.println("			</p>");
+		out.println("		</form>");
+
+		out.println("<p> <a href='Index.html'>Voltar ao Início</a> </p>");
+
+		out.println("	</div>");
+		out.println("</div>");
+
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
